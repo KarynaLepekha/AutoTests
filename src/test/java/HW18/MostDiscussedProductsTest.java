5a96ce28ab3322fb59d0e6db41f48d5df7777fbb
Karyna Lepekha <karyna_lepekha@epam.com>
2023-03-22 20:37:25 +0200

    hw 18 - lazy scroll updates

--- src/test/java/HW18/MostDiscussedProductsTest.java
+++ src/test/java/HW18/MostDiscussedProductsTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MostDiscussedProductsTest extends Driver{
@@ -10,18 +11,15 @@ public class MostDiscussedProductsTest extends Driver{
     public void findMostDiscussedProducts(){
         driver.get("https://rozetka.com.ua/ua/");
 
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        jse.executeScript("window.scrollBy(0,800)");
+        List<WebElement> products = new ArrayList<>();
+        while (products.size() == 0) {
+            List<WebElement> newProducts = driver.findElements(By.xpath("//h2[text() = ' Найбільш обговорювані товари ']/following-sibling::ul/li"));
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.scrollBy(0, document.body.scrollHeight)");
+            products.addAll(newProducts);
+            if (newProducts.size() != 0) break;
+            }
 
-        WebElement element = driver.findElement(By.xpath("//h2[contains(text(), 'Найчастіше додають в лист бажань')]"));
-        jse.executeScript("arguments[0].scrollIntoView();", element);
-
-        WebElement element2 = driver.findElement(By.xpath("(//h2[text() = ' Найбільш обговорювані товари ']" +
-                "//following-sibling::ul/li//a[@class = 'tile__title'])[1]"));
-        jse.executeScript("arguments[0].scrollIntoView();", element2);
-
-        List<WebElement> products = driver.findElements(By.xpath("//h2[text() = " +
-                "' Найбільш обговорювані товари ']//following-sibling::ul/li"));
         for (WebElement product : products) {
             String name = product.findElement(By.xpath(".//a[@class = 'tile__title']")).getAttribute("title");
             String price = product.findElement(By.xpath(".//div[contains(@class, 'tile__price ')]")).getText();
